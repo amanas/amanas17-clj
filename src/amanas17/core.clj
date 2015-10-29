dd0b941dfc84212ee59343bfaae9d0a728152d97
Andrés Mañas <andres.manas-manas@nokia.com>
2015-10-29 21:21:49 +0100

    adf

---
+++ src/amanas17/core.clj
@@ -0,0 +1,6 @@
+(ns amanas17.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/amanas17/core_test.clj
@@ -0,0 +1,7 @@
+(ns amanas17.core-test
+  (:require [clojure.test :refer :all]
+            [amanas17.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
