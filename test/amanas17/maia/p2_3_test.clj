a04253f2decf08160b7e5de45c57c417862f0bb7
Andrés Mañas <andresmanasmanas@gmail.com>
2016-03-18 13:46:53 +0100

    draft

--- test/amanas17/maia/p2_3_test.clj
+++ test/amanas17/maia/p2_3_test.clj
@@ -5,6 +5,8 @@
             [amanas17.maia.p2-2 :refer :all]
             [amanas17.maia.p2-3 :refer :all]))
 
+(def metadatos (first ejemplos))
+
 ;; Ejercicio 2.9 - tests
 ;; creo que el ejemplo del material de estudio está mal
 (assert (= [[[:niebla  :nublado :lluvioso :diluvia]  [*] [20] [:si]]
@@ -13,25 +15,25 @@
             [[:soleado :niebla  :nublado  :diluvia]  [*] [20] [:si]]
             [[:soleado :niebla  :nublado  :lluvioso] [*] [20] [:si]]]
            (especializaciones-atributo-nominal [[*][*][20][:si]] 0
-                                               (first ejemplos))))
+                                               metadatos)))
 (assert (= [[[][*][20][:si]]]
            (especializaciones-atributo-nominal [[:soleado][*][20][:si]] 0
-                                               [first ejemplos])))
+                                               metadatos)))
 (assert (= [[[][*][20][:si]]]
            (especializaciones-atributo-nominal [[][*][20][:si]] 0
-                                               [first ejemplos])))
+                                               metadatos)))
 
 ;; Ejercicio 2.10 - tests
 (assert (= [[[:soleado][*][20][:no]]
             [[:soleado][*][20][:si]]]
            (generalizaciones-atributo-nominal [[:soleado][*][20][]] 3
-                                              (first ejemplos))))
+                                              metadatos)))
 (assert (= [[[:soleado][*][20][*]]]
            (generalizaciones-atributo-nominal [[:soleado][*][20][:si]] 3
-                                              (first ejemplos))))
+                                              metadatos)))
 (assert (= [[[:soleado][*][20][*]]]
            (generalizaciones-atributo-nominal [[:soleado][*][20][*]] 3
-                                              (first ejemplos))))
+                                              metadatos)))
 
 ;; Ejercicio 2.11 - tests
 (assert (= [[:soleado][25][20][:si]]
@@ -58,8 +60,6 @@
                                                 [:soleado 25 40 :si :-])))
 
 ;; Ejercicio 2.13 - tests
-
-(def metadatos (first ejemplos))
 
 (assert
  (= [[[*][*][*][*] [*] [*] [*]]]
