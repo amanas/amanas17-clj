e16e69574271e02cc98ea9c198719154c7d72dca
amanas <andresmanasmanas@gmail.com>
2016-05-17 19:11:11 +0200

    draft

--- src/amanas17/maia/p2_9_2.clj
+++ src/amanas17/maia/p2_9_2.clj
@@ -9,6 +9,7 @@
         [amanas17.maia.p2-6]
         [amanas17.maia.p2-7]))
 
+
 (defn nuevo-conceptoNB
   "Crea un nuevo concepto Naive Bayes inicializado a 0 en todos sus valores"
   [metadatos]
@@ -40,6 +41,7 @@
               ([relajado 0] [estresado 0])
               ([solvente 0] [insuficiente 0]))]))
 
+;; Ejercicio 2.37
 (defn INB
   "Actualiza de modo incremental el concepto Naive Bayes con los datos del ejemplo"
   [conceptoNB ejemplo]
@@ -48,17 +50,94 @@
         update-NB (first (filter (fn [[clazz & more]] (= clase clazz)) conceptoNB))
         pairs (partition 2 (interleave (drop 2 update-NB) (butlast ejemplo)))
         updater (fn [[dist value]]
-                  (prn dist)
-                  (prn value)
-                  ;; (if (= 'numerico (first dist))
-                  ;;   [(nth dist 0) (+ (nth dist 1) value) (+ (nth dist 2) (Math/pow value 2))]
-                  ;;   (map (fn [[nombre contador]]
-                  ;;          [nombre (if (= nombre value) (inc contador) contador)])
-                  ;       dist
-                  )]
-    (prn update-NB)
-    (map updater pairs)))
+                  (if (= 'numerico (first dist))
+                    [(nth dist 0) (+ (nth dist 1) value) (+ (nth dist 2) (Math/pow value 2))]
+                    (map (fn [[nombre contador]]
+                           [nombre (if (= nombre value) (inc contador) contador)])
+                        dist)))]
+    (concat remain-NB [(concat [(first update-NB) (inc (second update-NB))] (map updater pairs))])))
+
+(he-tardado 60 2.37)
 
 
+;; Ejercicio 2.38
+(defn NB
+  "Devuelve el concepto NB correspondiente a los ejemplos"
+  [ejemplos]
+  (loop [conceptoNB (nuevo-conceptoNB (first ejemplos))
+         [ejemplo & more] (rest ejemplos)]
+    (if (nil? ejemplo)
+      conceptoNB
+      (recur (INB conceptoNB ejemplo) more))))
 
-(INB (nuevo-conceptoNB (first ejemplos)) (second ejemplos))
+(assert (= (NB ejemplos)
+           ['(-
+              11
+              ([soleado 1] [nublado 4] [lluvioso 6])
+              [numerico 276 7968.0]
+              [numerico 879 71181.0]
+              ([no 7] [si 4])
+              ([contento 5] [triste 6])
+              ([relajado 5] [estresado 6])
+              ([solvente 3] [insuficiente 8]))
+            '(+
+              9
+              ([soleado 3] [nublado 4] [lluvioso 2])
+              [numerico 242 6720.0]
+              [numerico 754 63524.0]
+              ([no 7] [si 2])
+              ([contento 9] [triste 0])
+              ([relajado 7] [estresado 2])
+              ([solvente 8] [insuficiente 1]))]))
+
+(he-tardado 10 2.38)
+
+
+;; Ejercicio 2.39
+
+(defn media
+  "Devuelve la media aritmética a partir de la suma de valores y el número de instancias"
+  [x n] (/ x n))
+
+(defn varianza
+  "Devuelve la varianza a partir de la suma de los cuadrados de los valores, la media
+   y el número de instancias"
+  [x2 m n]
+  (/ (+ x2 (* n (Math/pow m 2)) (* -2 m (* m n)))
+     (dec n)))
+
+(assert (= 2 (media 10 5)))
+(assert (= 2.5 (varianza 30 2 5)))
+
+(he-tardado 15 2.39)
+
+
+;; Ejercicio 2.40
+
+(defn norm-pdf
+  "Devuelve la probabilidad de v para una normal de media m y varianza x2"
+  [x m v]
+  (* (/ 1 (Math/sqrt (* 2 Math/PI v)))
+     (Math/exp (- (/ (Math/pow (- x m) 2)
+                     (* 2 v))))))
+
+(defn probabilidades
+  "Devuelve una lista con las probabilidad de la clase por atributo.
+   [P(c|vi)...]"
+  [clase conceptoNB ejemplo-sin-clase]
+  (let [[c n & more] (first (filter (fn [[clazz & more]] (= clase clazz)) conceptoNB))]
+    (loop [probs []
+           [att & atts] ejemplo-sin-clase
+           [dist & dists] more]
+      (if (nil? att)
+        probs
+        (do (if (= 'numerico (first dist))
+              (let [[_ x x2] dist
+                    m (media x n)]
+                (prn "num" (norm-pdf att m (varianza x2 m n))))
+              (prn "nom"))
+            (recur (concat probs [1]) atts dists)))
+      ;;    (for [i (range)])
+      )))
+
+(probabilidades '+ (NB ejemplos) (butlast (second ejemplos)))
