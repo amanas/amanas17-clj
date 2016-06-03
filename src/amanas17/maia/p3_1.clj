db7031128a7d643bd77f598a606e8750add2bcb5
amanas <andres.manas.manas@gmail.com>
2016-06-03 16:17:27 +0200

    draft

--- src/amanas17/maia/p3_1.clj
+++ src/amanas17/maia/p3_1.clj
@@ -1,9 +1,30 @@
 (ns amanas17.maia.p3-1
   (:use [amanas17.maia.p1-1]
         [amanas17.maia.p1-3]
+        [amanas17.maia.p1-4]
+        [amanas17.maia.p1-5]
+        [amanas17.maia.p1-6]
         [amanas17.maia.p2-1]
-        [amanas17.maia.p2-7]))
+        [amanas17.maia.p2-2]
+        [amanas17.maia.p2-4]
+        [amanas17.maia.p2-5]
+        [amanas17.maia.p2-6]
+        [amanas17.maia.p2-7]
+        [amanas17.maia.p2-9-0]
+        [amanas17.maia.p2-9-1]
+        [amanas17.maia.p2-9-2]))
 
+(defmacro LDim
+  "Macro de ayuda para interpretar evaluaciones en listas de decisión"
+  [concepto ejemplo-sin-clase]
+  `(let [[f# c# _ clazz#] ~concepto
+         f## (eval f#)
+         c## (condp = (name f#)
+               "match-LUU" [(try (eval (first c#)) (catch Exception ex# (first c#)))
+                            (try (eval (second c#)) (catch Exception ex# (second c#)))]
+               c#)
+         e# ~ejemplo-sin-clase]
+     (when (f## c## e#) clazz#)))
 
 ;; Ejercicio 3.1
 (defn LDi
@@ -11,26 +32,130 @@
   [concepto-LD ejemplo-sin-clase]
   (if (empty? concepto-LD)
     ejemplo-sin-clase
-    (let [[f c _ clazz] (first concepto-LD)]
-      (if ((eval f) c ejemplo-sin-clase)
-        (concat ejemplo-sin-clase [clazz])
+    (let [concepto (first concepto-LD)]
+      (if-let [c (LDim concepto ejemplo-sin-clase)]
+        (concat ejemplo-sin-clase [c])
         (LDi (rest concepto-LD) ejemplo-sin-clase)))))
 
-(assert (= (LDi '((match-CL ((soleado) (30 40) (50 60) (*)) => +)
-                   (match-CL ((*) (*) (*) (*)) => -))
-                '(soleado 30 40 si))
-           '(soleado 30 40 si -)))
+(he-tardado 240 3.1)
 
-(assert (= (LDi '((match-LUU ((first ejemplos) (0 1 1 0 -30)) => +)
-                    (match-LUU ((first ejemplos) (0 1 -1 0 -20)) => -))
-                '(soleado 30 40 si))
-           '(soleado 30 40 si +)))
+(assert (= (LDi '((match-CL ((soleado) (30 40) (50 60) (*) (*) (*) (*)) => +)
+                   (match-CL ((*) (*) (*) (*) (*) (*) (*)) => -))
+                '(soleado 30 40 si contento relajado solvente))
+           '(soleado 30 40 si contento relajado solvente -)))
 
-(~l)
+(assert (= (LDi '((match-LUU ((first ejemplos) (0 1 1 0 0 0 0 -30)) => +)
+                   (match-LUU ((first ejemplos) (0 1 -1 0 0 0 0 -20)) => -))
+                '(soleado 30 40 si contento relajado solvente))
+           '(soleado 30 40 si contento relajado solvente +)))
 
-(match-LUU '((first ejemplos) (0 1 1 0 -30))
-           '(soleado 30 40 si))
+(assert (= (LDi '((match-LUU ((first ejemplos) (0 1 1 0 0 0 0 30)) => +)
+                   (match-LUU ((first ejemplos) (0 1 -1 0 0 0 0 20)) => -))
+                '(soleado 10 10 si contento relajado solvente))
+           '(soleado 10 10 si contento relajado solvente)))
 
-(type (first '((first ejemplos) (0 1 1 0 -30))))
-(type (first '(gg (0 1 1 0 -30))))
-(he-tardado 60 3.1)
+
+;; Ejercicio 3.2
+(defn funcion-match
+  "Para cada algoritmo devuelve la función match asociada"
+  [algoritmo]
+  (condp = algoritmo
+    HGS match-CL
+    HTC match-CL
+    PCP match-LUU
+    LMS match-LUU
+    IB match-IB
+    NB match-NB))
+
+(assert (eval '((funcion-match HGS) '((soleado) (*)) '(soleado si))))
+
+(he-tardado 30 3.2)
+
+
+;; Ejercicio 3.3
+(defn NSC0
+  "Implementación no incremental de inducción de formas normales
+  disjuntivas (DNF) usando un enfoque de divide y vencerás"
+  [algoritmo PSET NSET DNF]
+  (loop [PSET PSET
+         NSET NSET
+         DNF DNF]
+    (if (empty? (rest PSET))
+      DNF
+      (let [concepto (algoritmo (concat PSET (rest NSET)))
+            newPSET (remove (fn [e] ((funcion-match algoritmo) concepto (butlast e))) (rest PSET))
+            ;; TODO: review
+            newPSET (if (= (count newPSET) (count (rest PSET))) [] newPSET)]
+        (recur (cons (first PSET) newPSET) NSET (concat DNF [concepto]))))))
+
+(defn NSC
+  [algoritmo ejemplos]
+  (let [meta (first ejemplos)
+        ej+ (->> ejemplos rest (filter (comp (partial = '+) last)))
+        ej- (->> ejemplos rest (filter (comp (partial = '-) last)))]
+    (NSC0 algoritmo (cons meta ej+) (cons meta ej-) [])))
+
+
+;; Ejercicio 3.4
+;Estudie la salida de la función NSC a partir de los datos
+;“buen díıa para salir al campo” y los datos “agaricus-lepiota” e “ionosphere”
+;procedentes de UCI mediante los algoritmos HGS, HTC, PCP y LMS. Comen-
+;te las diferencias, por ejemplo ¿qu ́e configuración devuelve listas de mayor
+;tamaño? ¿cuáles considera que podr ́ıan ofrecer mayor precisión?
+;; TODO
+(comment (NSC HGS all-ejemplos))
+(comment (NSC HTC all-ejemplos))
+(comment (NSC PCP all-ejemplos))
+(comment (NSC LMS all-ejemplos))
+(comment (NSC IB all-ejemplos))
+(comment (NSC NB all-ejemplos))
+
+;; Ejercicio 3.5
+(defn MSC0
+  "Algoritmo de inducción no incremental de listas de decisión multiclase"
+  [algoritmo ejemplos]
+  (let [meta (first ejemplos)
+        CLASS-SET (second (last (first ejemplos)))
+        RULE-SET (atom '())]
+    (for [CLASS CLASS-SET]
+      (let [PSET (cons meta (->> ejemplos rest (filter (fn [x] (= CLASS (last x))))))
+            NSET (cons meta (->> ejemplos rest (remove (fn [x] (= CLASS (last x))))))
+            DNF (NSC0 algoritmo PSET NSET [])]
+        (for [D DNF]
+          (do (prn D)
+              (swap! RULE-SET concat (list (funcion-match algoritmo) D '=> CLASS))))))))
+
+(defn MSC
+  "Añade a la lista de decisión devuelta por MSC0 un valor por defecto"
+  [algoritmo ejemplos]
+  (concat (MSC0 algoritmo ejemplos)
+          (list match-CL (repeat (dec (count (first ejemplos))) '(*)) '=> (A0 ejemplos)))) )
+
+(he-tardado 60 3.5)
+
+
+;; Ejercicio 3.6
+;; Estudie la eficiencia de MSC mediante distintas configuraciones variando
+;; el algoritmo introducido como parámetro: HGS, HTC, PCP y LMS.
+;; Para ello, denomine cada nueva función como MSC-<subalgoritmo> (p.ej. MSC-HGS).
+;;
+;; Tras ello, compruebe la eficiencia de cada uno mediante ten-fold-crossvalidation
+;; y los datos “buen d ́ıa para salir al campo”, “agaricus-lepiota”, “ionosphere”,
+;; “poker” y “lymphography”. ¿Qu ́e configuración produce mejores resultados?
+;; ¿Por qué cree que es así?.
+
+;; TODO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/amanas17/maia/p3_2.clj
@@ -0,0 +1,135 @@
+(ns amanas17.maia.p3-2
+  (:use [amanas17.maia.p1-1]
+        [amanas17.maia.p1-3]
+        [amanas17.maia.p1-4]
+        [amanas17.maia.p1-5]
+        [amanas17.maia.p1-6]
+        [amanas17.maia.p2-1]
+        [amanas17.maia.p2-2]
+        [amanas17.maia.p2-4]
+        [amanas17.maia.p2-5]
+        [amanas17.maia.p2-6]
+        [amanas17.maia.p2-7]
+        [amanas17.maia.p2-9-0]
+        [amanas17.maia.p2-9-1]
+        [amanas17.maia.p2-9-2]))
+
+;; Ejercicio 3.7
+(defn adc
+  "Elige la rama apropiada"
+  [ejemplo-sin-clase ramas]
+  (or (->> (for [[concepto _ subrama] ramas]
+             (when (match-CL concepto ejemplo-sin-clase) subrama))
+           (remove nil?)
+           first)
+      '()))
+
+
+(assert (= (adc '(soleado 20)
+                '((((soleado) (*)) -> (adc (((*) (-inf 30)) -> (=> +))
+                                           (((*) ((30) +inf)) -> (=> -))))
+                   (((nublado) (*)) -> (=> -))
+                   (((lluvioso) (*)) -> (adc (((*) (-inf 10)) -> (=> -))
+                                             (((*) ((10) +inf)) -> (=> +))))))
+           '(adc (((*) (-inf 30)) -> (=> +))
+                 (((*) ((30) +inf)) -> (=> -)))))
+
+(assert (= (adc '(soleado 20)
+                '((((*) (-inf 30)) -> (=> +))
+                   (((*) ((30) +inf)) -> (=> -))))
+           '(=> +)))
+
+(he-tardado 60 3.7)
+
+;; Ejercicio 3.8
+(def JC-multivariado-equiv '((((soleado) (-inf 30)) -> (=> +))
+                              (((soleado) ((30) +inf)) -> (=> -))
+                              (((nublado) (*)) -> (=> -))
+                              (((lluvioso) (-inf 10)) -> (=> -))
+                              (((lluvioso) ((10) +inf)) -> (=> -))))
+
+(he-tardado 20 3.8)
+
+
+;; Ejercicio 3.9
+
+(defn adg
+  "Método de elección de ramo para árboles de decisión generalizados"
+  [ejemplo-sin-clase ramas]
+  (or (->> (for [[[f-match concepto] _ subrama] ramas]
+             (let [eval-concepto (condp = (name f-match)
+                                   "match-LUU" [(try (eval (first concepto)) (catch Exception e (first concepto)))
+                                                (try (eval (second concepto)) (catch Exception e (second concepto)))]
+                                   concepto)]
+               (when ((eval f-match) eval-concepto ejemplo-sin-clase)
+                 subrama)))
+           (remove nil?)
+           first)
+      '()))
+
+(assert (= (adg '(soleado 20)
+                '(((match-CL ((soleado) (*))) ->
+                    (adg ((match-LUU ((map (vec (first ejemplos)) [0 1 7]) (1 2 -10))) -> (=> +))
+                         ((match-LUU ((map (vec (first ejemplos)) [0 1 7]) (1 2 10))) -> (=> -))))
+                   ((match-CL ((nublado) (*))) -> (=> -))))
+           '(adg ((match-LUU ((map (vec (first ejemplos)) [0 1 7]) (1 2 -10))) -> (=> +))
+                 ((match-LUU ((map (vec (first ejemplos)) [0 1 7]) (1 2 10))) -> (=> -)))))
+
+(assert (= (adg '(soleado 20)
+                '(((match-LUU ((map (vec (first ejemplos)) [0 1 7]) (1 2 50))) -> (=> +))
+                   ((match-LUU ((map (vec (first ejemplos)) [0 1 7]) (1 2 -10))) -> (=> -))))
+           '(=> -)))
+
+(he-tardado 120 3.9)
+
+
+;; Ejercicio 3.10
+(defn JCi
+  "Intérprete de Jerarquías de conceptos"
+  [[f & ramas :as concepto-JC] ejemplo-sin-clase]
+  (if (empty? concepto-JC)
+    ejemplo-sin-clase
+    (let [result ((eval f) ejemplo-sin-clase ramas)]
+      (if (= '=> (first result))
+        (concat ejemplo-sin-clase [(second result)])
+        (JCi result ejemplo-sin-clase)))))
+
+
+(assert (= (JCi '(adg
+                   ((match-CL ((soleado) (*))) ->
+                     (adg
+                       ((match-CL ((*) (-inf 30))) -> (=> +))
+                       ((match-CL ((*) ((30) +inf))) -> (=> -))))
+                   ((match-CL ((nublado) (*))) -> (=> -))
+                   ((match-CL ((lluvioso) (*))) ->
+                     (adg
+                       ((match-CL ((*) (-inf 10))) -> (=> -))
+                       ((match-CL ((*) ((10) +inf))) => (=> +)))))
+                '(soleado 20))
+           '(soleado 20 +)))
+(assert (= (JCi '(adc
+                   (((soleado) (*)) ->
+                     (adc
+                       (((*) (-inf 30)) -> (=> +))
+                       (((*) ((30) +inf)) -> (=> -))))
+                   (((nublado) (*)) -> (=> -))
+                   (((lluvioso) (*)) ->
+                     (adc
+                       (((*) (-inf 10)) -> (=> -))
+                       (((*) ((10) +inf)) -> (=> +)))))
+                '(lluvioso 50))
+           '(lluvioso 50 +)))
+(assert (= (JCi '(adc
+                   (((soleado) (*)) ->
+                     (adc
+                       (((*) (-inf 30)) -> (=> +))
+                       (((*) ((30) +inf)) -> (=> -))))
+                   (((nublado) (*)) -> (=> -))
+                   (((lluvioso) (*)) ->
+                     (adc
+                       (((*) (-inf 10)) -> (=> -))
+                       (((*) ((10) +inf)) -> ()))))
+                '(lluvioso 50))
+           '(lluvioso 50)))
+
+(he-tardado 60 3.10)
